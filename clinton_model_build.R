e262bb8f4980947b7a4f487e1da526130017d840
briannaeskin <brianna.eskin@yahoo.com>
2020-11-21 11:51:23 -0500

    Created test and train data

--- clinton_model_build.R
+++ clinton_model_build.R
@@ -1,2 +1,11 @@
 #read data
 clinton <- read.csv("https://raw.githubusercontent.com/briannaeskin/RegressionAndTimeSeriesFall2020/main/ClintonElectionData1992.csv")
+
+#split to test and train data
+sample_size <- floor(0.8*nrow(clinton))
+set.seed(777)
+
+data_sample <- sample(seq_len(nrow(clinton)),size=sample_size)
+train_clinton <- clinton[data_sample,]
+test_clinton <- clinton[-data_sample,]
+
