98e9694eab5bc68b28ca7a82755da824809f92da
briannaeskin <brianna.eskin@yahoo.com>
2020-11-21 12:41:22 -0500

    tweak csv read to create counties as row names Add code to identify potential leverage points

--- clinton_model_build.R
+++ clinton_model_build.R
@@ -1,5 +1,15 @@
+library("faraway")
+
 #read data
-clinton <- read.csv("https://raw.githubusercontent.com/briannaeskin/RegressionAndTimeSeriesFall2020/main/ClintonElectionData1992.csv")
+clinton <- read.csv("https://raw.githubusercontent.com/briannaeskin/RegressionAndTimeSeriesFall2020/main/ClintonElectionData1992.csv", header = TRUE, row.names = 1)
+
+#Leverage/Outliers/Influential (Full Data)
+#Leverage
+lmod_clinton <- lm(VotingPct ~ MedAge + Savings + PerCapIncome + Poverty + Veterans + Female + PopDensity + NursingHome + Crime, clinton)
+hatv <- hatvalues(lmod_clinton)
+counties <- row.names(clinton)
+halfnorm(hatv, labs=counties,ylab="Leverages")
+
 
 #split to test and train data
 sample_size <- floor(0.8*nrow(clinton))
